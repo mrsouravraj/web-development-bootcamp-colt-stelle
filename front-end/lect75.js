73e0b3ea2e7dbba2947484299dc91e5469b0f129
mrsouravraj <mr.sourav.raj@gmail.com>
2017-09-10 01:07:59 +0530

    Section 8 Basic Java Script

---
+++ front-end/lect75.js
@@ -0,0 +1,5 @@
+var first=prompt("Your first Name?");
+var last=prompt("Your Last Name?");
+var age=prompt("Your age?");
+console.log("Your full name is "+first+" "+last)
+console.log("Your age is "+age+" years old")
---
+++ front-end/lect77.js
@@ -0,0 +1,3 @@
+var age=prompt("How old are you");
+var days=age*365.25;
+alert(age+" yeas is roughly "+days+" day)
