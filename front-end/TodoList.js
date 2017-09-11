85c34b894fdd0ac0565b3fb4706c4fd4e1cdd07b
mrsouravraj <mr.sourav.raj@gmail.com>
2017-09-11 23:55:26 +0530

    v2 of Todo List

--- front-end/TodoList.js
+++ front-end/TodoList.js
@@ -1,14 +1,40 @@
-var todos=[];
+var todos=["Buy new Turtle"];
 var input=prompt("What would you like to do?");
 
 while(input != "quit"){
-		if(input == "list"){
-		console.log(todos); 
+		if(input === "list"){
+			listTodos();
 	}
-	else if(input == "new"){
-		var add = prompt("Enter new todo");
-		todos.push(add);
+	else if(input === "new"){
+		addTodo();
 	}
-		var input=prompt("What would you like to do?");
+	else if (input === "delete") {
+	   deleteTodo();
+    }
+	input=prompt("What would you like to do?");
 }
+
 console.log("OK, YOU QUIT THE APP");
+
+function listTodos() {
+	console.log("**********");
+		todos.forEach(function(todo,i){
+			console.log(i + ":" +todo);
+		});
+		console.log("**********");
+}
+
+function addTod() {
+	var add = prompt("Enter new todo");
+		todos.push(add);
+		console.log("Added Todo");
+}
+
+function deleteTodo() {
+	//ask for index of todo to be deletes
+	var index=prompt("Enter index of todo to delete");
+	// delete that todo 
+	//splice()
+	todos.splice(index,1);
+	console.log("Todo Deleted");
+}
